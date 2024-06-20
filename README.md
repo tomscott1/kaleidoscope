c14d75808a81626bc27cc60daa150ef704813b50
Tom Scott <tomscott85@gmail.com>
2024-06-20 18:58:26 +1000

    update navbar styles

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Kaleidoscope Front End Portfolio Project
 
 Built from:
-https://www.figma.com/community/file/1310560561025920170/kaleidoscope-video-production-portfolio-website-freebie-ver
+https://www.figma.com/community/file/1310560561025920170/kaleidoscope-video-production-portfolio-website-freebie-vergit 
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import "./styles.css"
+import React from 'react';
+import './styles.css';
 import logo from '../../assets/logo.svg'; // Adjust the path if needed
 
 class Navbar extends React.Component {
-
   render() {
     return (
       <div>
-        <nav class="navbar">
-          <div class="logo">
+        <nav className="navbar">
+          <div className="logo">
             <img src={logo} alt="Logo" />
           </div>
-          <div class="menu">
-              <a href="#">Home</a>
-              <a href="#">About Us</a>
-              <a href="#">Services</a>
-              <a href="#">Portfolios</a>
-              <a href="#">Contact Us</a>
+          <div className="menu">
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Services</a>
+            <a href="#">Portfolios</a>
+            <a href="#">Contact Us</a>
           </div>
-          <a href="#" class="cta">Book A Demo</a>
+          <a href="#" className="cta">Book A Demo</a>
         </nav>
       </div>
-    )
+    );
   }
 }
 
-export default Navbar
+export default Navbar;
